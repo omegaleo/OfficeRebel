056494e833dd5ba74a28b33731659d3f8ebf496d
Werghoul <107267596+Werghoul@users.noreply.github.com>
2023-04-13 20:44:17 +0200

    PointUI
    Added UI display for number of items stolen and value

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -7,19 +7,20 @@ public class HUDManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] TextMeshProUGUI pointsText;
-    [SerializeField] TextMeshProUGUI warningText;
 
     [SerializeField] WorkDayTimer timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
         timerText.text = timer.FormatTimeDisplay();
+        pointsText.text = "Value: " + Player.Instance.Money.ToString() + "\n" +
+                          "Stolen: " + Player.Instance.ItemsStolen.ToString();
     }
 }
